12eaf6556259b0a5a1e150db88d722a7eb295334
Subin Siby <mail@subinsb.com>
2020-12-18 21:05:53 +0530

    Add web frontend

--- adbtv.py
+++ adbtv.py
@@ -1,5 +1,15 @@
 import eel
+import os
 
+state = {
+    "active": True
+}
 
-eel.init('web/public')
+@eel.expose
+def setState(newState):
+    state.update(newState)
+    print(state)
+    eel.updateState(state)
+
+eel.init(os.path.join(os.path.realpath(os.path.dirname(__file__)), 'web', 'public'))
 eel.start('index.html', mode=False, host="0.0.0.0", port=8100)
---
+++ web/src/App.svelte
@@ -0,0 +1,53 @@
+<svelte:head>
+  <title>ADBTV</title>
+</svelte:head>
+
+<main>
+  <TopAppBar variant="static" color="primary">
+    <Row>
+      <Section>
+        <IconButton class="material-icons">menu</IconButton>
+        <Title>AdBlock TV</Title>
+      </Section>
+      <Section align="end" toolbar>
+        {#if !state.active}
+          <IconButton class="material-icons" aria-label="Download" on:click={resume}>play_circle</IconButton>
+        {:else}
+          <IconButton class="material-icons" aria-label="Download" on:click={pause}>pause_circle</IconButton>
+        {/if}
+      </Section>
+    </Row>
+  </TopAppBar>
+</main>
+
+<script>
+  import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+  
+  let state = {
+    active: true
+  };
+
+  function resume() {
+    eel.setState({
+      active: true
+    })
+  }
+
+  function pause() {
+    eel.setState({
+      active: false
+    })
+  }
+
+  function updateState(newState) {
+    state = newState;
+  }
+  eel.expose(updateState);
+</script>
+
+<style>
+  main {
+    margin: -8px;
+  }
+</style>
