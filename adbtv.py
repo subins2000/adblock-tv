943c020b7b6d0a6c1e3084a1c1a72d4daa51ee75
Subin Siby <mail@subinsb.com>
2020-12-19 00:03:33 +0530

    Enable/disable adblocker works

--- adbtv.py
+++ adbtv.py
@@ -33,17 +33,19 @@ def unmute():
         muted = False
 
 
-def main():
-    eel.init(os.path.join(os.path.realpath(os.path.dirname(__file__)), 'web', 'public'))
-    eel.start('index.html', mode=False, host="0.0.0.0", port=8100, block=False)
-
+def scan():
+    eel.sleep(1.0)
     adbtv = ADBTV(dejavu_config, "ads/", [".mp3", ".wav"], True)
 
     while True:
+        if not state["active"]:
+            eel.sleep(1.0)
+            continue
+
         try:
             result = adbtv.record(2, 0.15)
 
-            if result is not False:
+            if type(result) is tuple:
                 sn = str(result['song_name'])
                 inco = result['input_confidence']
                 fco = result['fingerprinted_confidence']
@@ -52,8 +54,17 @@ def main():
                 mute()
             else:
                 unmute()
-        except:
+        except NoSound:
             pass
+        
+        eel.sleep(0.01)
+
+
+def main():
+    eel.init(os.path.join(os.path.realpath(os.path.dirname(__file__)), 'web', 'public'))
+
+    eel.spawn(scan)
+    eel.start('index.html', mode=False, host="0.0.0.0", port=8100)
 
 
 if __name__ == "__main__":
