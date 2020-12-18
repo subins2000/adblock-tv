a952e6cf17cc6df02af231681122376052cfd0f0
Subin Siby <mail@subinsb.com>
2020-12-18 14:14:51 +0530

    Convert to mp3

--- record-ad.py
+++ record-ad.py
@@ -1,10 +1,14 @@
+from array import array
+import os
 import pyaudio
+from pydub import AudioSegment
 import wave
 
-CHUNK = 1024
+CHUNK = 512
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
+CHANNELS = 1
 RATE = 44100
+MIN_VOLUME = 300
 
 p = pyaudio.PyAudio()
 
@@ -21,19 +25,32 @@ frames = []
 try:
     while True:
         data = stream.read(CHUNK)
-        frames.append(data)
+        data_chunk = array('h', data)
+        vol = max(data_chunk)
+
+        if vol >= MIN_VOLUME:
+            frames.append(data)
 except KeyboardInterrupt:
     print("* done recording")
 
-    WAVE_OUTPUT_FILENAME = input("Ad name: ") + ".wav"
+    WAVE_OUTPUT_FILEPATH = "ads/" + input("Ad name: ")
 
     stream.stop_stream()
     stream.close()
     p.terminate()
 
-    wf = wave.open("ads/" + WAVE_OUTPUT_FILENAME, "wb")
+    wf = wave.open(WAVE_OUTPUT_FILEPATH + ".wav", "wb")
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(FORMAT))
     wf.setframerate(RATE)
     wf.writeframes(b''.join(frames))
     wf.close()
+
+    print("Converting to MP3...")
+
+    sound = AudioSegment.from_wav(WAVE_OUTPUT_FILEPATH + ".wav")
+    sound.export(WAVE_OUTPUT_FILEPATH + ".mp3", format='mp3')
+
+    os.remove(WAVE_OUTPUT_FILEPATH + ".wav")
+
+    print("Done")
